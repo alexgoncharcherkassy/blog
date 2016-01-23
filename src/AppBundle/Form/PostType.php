80c076af5d2fffce7ae89ddd2620b64f53280062
Aleksandr Gonchar <shuric2005@mail.ru>
2016-01-23 19:02:02 +0200

    add magicsuggest

--- src/AppBundle/Form/PostType.php
+++ src/AppBundle/Form/PostType.php
@@ -2,8 +2,6 @@
 
 namespace AppBundle\Form;
 
-use Doctrine\ORM\EntityRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -30,44 +28,11 @@ class PostType extends AbstractType
                 ]
             ])
             ->add('newTags', TextType::class, [
-                /*'attr' => [
-                    'class' => 'form-control',
-                    'placeholder' => 'Add tags through a comma or/and select from tags'
-                ],*/
                 'required' => false
             ])
-            /*->add('tags', EntityType::class, [
-                'class' => 'AppBundle\Entity\Tag',
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('t')
-                        ->orderBy('t.tagName', 'ASC');
-                },
-                'choice_label' => 'tagName',
-                'multiple' => true,
-                'expanded' => true,
-                'attr' => ['class' => 'list-group-item'],
-                'required' => false
-            ])*/
             ->add('newCategory', TextType::class, [
-               /* 'attr' => [
-                    'class' => 'form-control',
-                    'placeholder' => 'Add only one category or select from categories'
-                ],*/
                 'required' => false
             ])
-           /* ->add('category', EntityType::class, [
-                'class' => 'AppBundle\Entity\Category',
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('c')
-                        ->orderBy('c.categoryName', 'ASC');
-                },
-                'choice_label' => 'categoryName',
-                'attr' => [
-                    'class' => 'form-control'
-                ],
-                'placeholder' => 'Select category',
-                'required' => false
-            ])*/
             ->add('blog_image', FileType::class, [
                 'required' => false
             ]);
