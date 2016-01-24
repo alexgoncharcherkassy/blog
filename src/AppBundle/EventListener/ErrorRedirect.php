155b99b8541d0967c8fd4c4d0e0137d09c9d7808
Aleksandr Gonchar <shuric2005@mail.ru>
2016-01-24 11:57:07 +0200

    add eventlistener errorredirect

---
+++ src/AppBundle/EventListener/ErrorRedirect.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: device
+ * Date: 24.01.16
+ * Time: 11:49
+ */
+
+namespace AppBundle\EventListener;
+
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+
+class ErrorRedirect
+{
+    protected $router;
+
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+
+        if ($exception instanceof NotFoundHttpException) {
+
+            $route = 'page404';
+
+            if ($route === $event->getRequest()->get('_route')) {
+                return;
+            }
+
+            $url = $this->router->generate($route);
+            $response = new RedirectResponse($url);
+            $event->setResponse($response);
+
+        }
+    }
+}
